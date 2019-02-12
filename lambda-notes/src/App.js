3afd65c990a69708a9e2d31c257df2998f6ac13c
Noah <noah.stoeckel@gmail.com>
2019-02-11 23:40:18 -0500

    Adds State To App
    - Creates fetchNotes method

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import axios from "axios";
 import "./App.css";
 import { SideBar } from "./components";
+import { ListView } from "./views";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notes: [],
+      error: ""
+    };
+  }
+
+  componentDidMount() {
+    this.fetchNotes();
+  }
+
+  fetchNotes = () => {
+    axios
+      .get(`https://fe-notes.herokuapp.com/note/get/all`)
+      .then(({ data }) => this.setState({ notes: data }))
+      .catch(error => this.setState({ error: error }));
+  };
+
   render() {
     return (
       <div className="App">
