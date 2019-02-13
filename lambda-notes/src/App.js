6954c82a89d394559fb66a95120f68db37e21774
Noah <noah.stoeckel@gmail.com>
2019-02-12 21:55:39 -0500

    Styles The Application Wrapper
    - Adds colors
    - Adds padding and margins

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -7,10 +7,34 @@ import { SideBar } from "./components";
 import { ListView } from "./views";
 
 const ApplicationWrapper = styled.div`
+  /*sidebar  */
   > div {
     background: red;
     position: fixed;
     height: 100%;
+    width: 180px;
+    padding: 10px;
+    background-color: #d3d2d3;
+
+    display: flex;
+    flex-direction: column;
+
+    & h1 {
+      font-size: 2rem;
+      font-weight: 600;
+      margin-bottom: 25px;
+    }
+  }
+
+  /*  main content */
+  > section {
+    margin-left: 180px;
+    padding: 55px 20px 0 20px;
+    background-color: #f2f1f2;
+
+    & h2 {
+      font-size: 1.2rem;
+    }
   }
 `;
 
