9bb9b28a75f2cdb66d26e402b50b383a2075e9f8
Noah <noah.stoeckel@gmail.com>
2019-02-13 00:30:14 -0500

    Fixes Long Singular Strings Breaking Layout

--- lambda-notes/src/styles/NoteCardWrapper.js
+++ lambda-notes/src/styles/NoteCardWrapper.js
@@ -2,9 +2,16 @@ import styled from "styled-components";
 
 //  Wraps Each Individual Card, used in NoteCard Component
 const NoteCardWrapper = styled.div`
+  /* DANG this solves the problem of 
+    super long strings breaking the layout perfectly!
+    ref: https://codepen.io/anon/pen/MPQZGB
+   */
+  word-wrap: break-word;
+
   border: 1px solid black;
   padding: 10px;
   min-height: 200px;
+  overflow: hidden;
 
   & h3 {
     font-weight: bolder;
--- lambda-notes/src/styles/NoteCardsWrapper.js
+++ lambda-notes/src/styles/NoteCardsWrapper.js
@@ -3,8 +3,16 @@ import styled from "styled-components";
 // wraps all of the note cards goes inside note list
 const NoteCardsWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  /* 
+    https://stackoverflow.com/questions/43311943/prevent-content-from-expanding-grid-items
+    https://stackoverflow.com/questions/52861086/how-come-minmax0-1fr-works-for-long-elements-while-1fr-doesnt
+    https://stackoverflow.com/questions/52785750/prevent-grid-area-from-expanding-causing-whole-page-to-scroll
+   */
+  /* By default grid has a min of 'auto' this sets it to zero */
+  grid-template-columns: repeat(3, minmax(0, 1fr));
   grid-gap: 10px;
+  grid-auto-flow: columns;
+  grid-auto-columns: 1fr;
 `;
 
 export default NoteCardsWrapper;
