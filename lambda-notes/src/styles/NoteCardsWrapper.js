82a9003155d50bde033105dc204045339f98260d
Noah <noah.stoeckel@gmail.com>
2019-02-12 22:43:50 -0500

    Adds Comments To NoteCardsWrapper

--- lambda-notes/src/styles/NoteCardsWrapper.js
+++ lambda-notes/src/styles/NoteCardsWrapper.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+// wraps all of the note cards goes inside note list
 const NoteCardsWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
