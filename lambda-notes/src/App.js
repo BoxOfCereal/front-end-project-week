38d4777529fc140b0431db8452809fbb94390be7
Noah <noah.stoeckel@gmail.com>
2019-02-12 22:14:13 -0500

    Refactors Styled Components Into Their Own Directory

--- lambda-notes/src/App.js
+++ lambda-notes/src/App.js
@@ -1,42 +1,10 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import axios from "axios";
-import styled from "styled-components";
-import "./App.css";
+
 import { SideBar } from "./components";
 import { ListView } from "./views";
-
-const ApplicationWrapper = styled.div`
-  /*sidebar  */
-  > div {
-    background: red;
-    position: fixed;
-    height: 100%;
-    width: 180px;
-    padding: 10px;
-    background-color: #d3d2d3;
-
-    display: flex;
-    flex-direction: column;
-
-    & h1 {
-      font-size: 2rem;
-      font-weight: 600;
-      margin-bottom: 25px;
-    }
-  }
-
-  /*  main content */
-  > section {
-    margin-left: 180px;
-    padding: 55px 20px 0 20px;
-    background-color: #f2f1f2;
-
-    & h2 {
-      font-size: 1.2rem;
-    }
-  }
-`;
+import { ApplicationWrapper } from "./styles/index";
 
 class App extends Component {
   constructor() {
