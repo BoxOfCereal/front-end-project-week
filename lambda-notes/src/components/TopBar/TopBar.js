ff2c73d56bbac46f3d09ce5a9b21d2691c33c2a7
Noah <noah.stoeckel@gmail.com>
2019-02-14 19:49:44 -0500

    Add Routes To Top Bar Based On Location

--- lambda-notes/src/components/TopBar/TopBar.js
+++ lambda-notes/src/components/TopBar/TopBar.js
@@ -1,11 +1,38 @@
 import React from "react";
+import { Route } from "react-router-dom";
+
+const routes = [
+  {
+    path: "/",
+    exact: true,
+    topbar: () => <h2>Your Notes:</h2>
+  },
+  {
+    path: "/create",
+    topbar: () => <h2>Create New Note:</h2>
+  },
+  {
+    path: "/note/:id",
+    topbar: () => <h2>Note Title</h2>
+  },
+  {
+    path: "/note/:id/edit",
+    topbar: () => <h2>Edit Note:</h2>
+  }
+];
 
 //https://reacttraining.com/react-router/web/example/sidebar
-const TopBar = props => {
+const TopBar = (route, index) => {
   return (
     <div>
-      {/* <h2>{props.pageName}</h2> */}
-      <h2>Your Notes:</h2>
+      {routes.map(route => (
+        <Route
+          key={index}
+          path={route.path}
+          exact={route.exact}
+          component={route.topbar}
+        />
+      ))}
     </div>
   );
 };
