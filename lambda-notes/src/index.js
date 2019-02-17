eb71bef047c1ff526e1b54674e5acdab72d20c47
Noah <noah.stoeckel@gmail.com>
2019-02-16 19:03:02 -0500

    Import Redux Logger

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -7,11 +7,12 @@ import App from "./App";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import logger from "redux-logger";
 // import rootReducer from "./reducers";
 
 const store = createStore(
   () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
-  applyMiddleware(thunk)
+  applyMiddleware(thunk, logger)
 );
 
 ReactDOM.render(
