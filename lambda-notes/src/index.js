65fee5132f977b0da886b3ea3b9a9c578984c7f2
Noah <noah.stoeckel@gmail.com>
2019-02-16 19:01:10 -0500

    Set Up For Redux ReFactor
    - Install
     - redux
     - react-redux
     - redux-logger
     - redux-thunk
    - import libraries into index
    - wrap application with provider

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -4,9 +4,21 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+// import rootReducer from "./reducers";
+
+const store = createStore(
+  () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
+  applyMiddleware(thunk)
+);
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
