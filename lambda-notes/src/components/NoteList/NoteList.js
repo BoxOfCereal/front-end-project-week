3d70de758895d92d0b576b4d5b911b131753c3cf
Noah <noah.stoeckel@gmail.com>
2019-02-22 19:40:33 -0500

    Filters Note List Based Off Of The Search Term

--- lambda-notes/src/components/NoteList/NoteList.js
+++ lambda-notes/src/components/NoteList/NoteList.js
@@ -1,17 +1,31 @@
 import React from "react";
+import { connect } from "react-redux";
 import { NoteCard } from "../index";
 import { NoteCardsWrapper } from "../../styles";
 
 //here is where I wanna filter my notes by the search term
 
 const NoteList = props => {
+  console.log(props);
+  const re = new RegExp(props.searchTerm, "i");
   return (
     <NoteCardsWrapper>
-      {props.notes.map(note => (
-        <NoteCard key={note._id} {...note} />
-      ))}
+      {props.notes
+        .filter(n => {
+          return n.title.match(re);
+        })
+        .map(note => (
+          <NoteCard key={note._id} {...note} />
+        ))}
     </NoteCardsWrapper>
   );
 };
 
-export default NoteList;
+const mstp = ({ notesReducer: state }) => {
+  return { searchTerm: state.searchTerm };
+};
+
+export default connect(
+  mstp,
+  {}
+)(NoteList);
